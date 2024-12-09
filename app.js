78aea08accbe2b618c469bcf263ac12273c3c902
Prabhas-Chilla <prabhachilla056@gmail.com>
2024-12-10 01:21:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express=require('express');
 const app=express();
 app.get('/',(req,res)=>{
-  res.send("Hello world anand");
+  res.send("Hello world anand How are you");
 }
 );
 app.listen(30,()=>{
